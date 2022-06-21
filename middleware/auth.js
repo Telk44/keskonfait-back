293912d60f6b834ff97a1f7ce0e4bf93df1cca79
Kristell <klansonneur@lmwr.fr>
2022-06-21 14:34:10 +0200

    middleware email-validator

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,11 +15,6 @@ module.exports = (req, res, next) => {
             throw 'User ID non valable';
         } else {
             next()
-            //on vérifie que l'UserId est dans la BDD
-            // User.findOne({ where: { id: userId } }).then(user => {
-            //     req.user = user
-            //     next()
-            // })
         }
     } catch (error) {
         res.status(401).json({
@@ -29,30 +24,3 @@ module.exports = (req, res, next) => {
 }
 
 
-// const jwt = require("jsonwebtoken");
-// module.exports = {
-//     validateRegister: (req, res, next) => {
-//         // username min length 3
-//         if (!req.body.username || req.body.username.length < 3) {
-//             return res.status(400).send({
-//                 msg: 'Please enter a username with min. 3 chars'
-//             });
-//         }
-//         // password min 6 chars
-//         if (!req.body.password || req.body.password.length < 6) {
-//             return res.status(400).send({
-//                 msg: 'Please enter a password with min. 6 chars'
-//             });
-//         }
-//         // password (repeat) does not match
-//         if (
-//             !req.body.password_repeat ||
-//             req.body.password != req.body.password_repeat
-//         ) {
-//             return res.status(400).send({
-//                 msg: 'Both passwords must match'
-//             });
-//         }
-//         next();
-//     }
-// };
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 const auth = require('../middleware/auth');
+const userMiddleware = require('../middleware/user');
 
-router.post('/signup', userCtrl .signup);
+router.post('/signup', userMiddleware, userCtrl .signup);
 router.post('/login', userCtrl .login);
 router.delete('/:id', auth, userCtrl .deleteAccount);
 router.get('/:id', auth, userCtrl .getOneAccount);
